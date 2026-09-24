7b09ea5643513453049af05d72697a73432d8e98
Kadesh Stewart <kadesh@kadeshstewart.com>
2026-09-24 12:14:48 -0400

    saving project progress

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,17 +1,33 @@
-import os
+import os, sys
+from stat import *
 
 def get_files_info(working_directory, directory="."): 
     working_dir_abs = os.path.abspath(os.getcwd())
+    print("Working dir: " + working_dir_abs)
     target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
-    # find the common directoty 
-    valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
-    if valid_target_dir == False:
-        f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    print("Target dir: " + target_dir)
+
+    #check if there is a valid common dir (should be a value larger than '/')
+    valid_common_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
+
+    if directory == ".":
+        print("Result for current directory: ")
+    else:
+        print(f"Result for '{directory}' directory: ")
+
     
-    # iterate over each item in the directory
-    dir_list = os.listdir(directory)
-    dir_list.sort()
-    for item in dir_list:
-        print(item)
-        #  +": " +"file_size="+ str(os.stat(item).st_size)+"bytes, is_dir="
-        # str(os.DirEntry.is_dir(item)
+    if not os.path.isdir(directory): 
+        print(f'    Error: "{directory}" is not a directory')
+
+    if not valid_common_dir:
+        print(f'    Error: Cannot list "{directory}" as it is outside the permitted working directory')
+    else: 
+        print(f'    Success: "{directory}" is within the working directory')
+        # walk the dir 
+
+        for f in os.listdir(directory): 
+            pathname = os.path.join(directory, f)
+            if os.path.isdir(directory): 
+                print(f'    - {f}: file_size={os.path.getsize(f)}, is_dir={os.path.isdir(f)}')
+            else: 
+                print(f'    - {f}: file_size={os.path.getsize(f)}, is_dir={os.path.isdir(f)}')
--- functions/test_get_files_info.py
+++ functions/test_get_files_info.py
@@ -1,6 +1,6 @@
 import get_files_info
 
-# get_files_info.get_files_info("calculator", ".")
-# get_files_info.get_files_info("calculator", "pkg")
+get_files_info.get_files_info("calculator", ".")
+get_files_info.get_files_info("calculator", "pkg")
 get_files_info.get_files_info("calculator", "/bin")
-# get_files_info.get_files_info("calculator", "../")
+get_files_info.get_files_info("calculator", "../")
